c3c2b74e5086c250015257966929a777c4c5f94a
MagmaGuy <tiagoarnaut@gmail.com>
2017-04-26 18:45:56 +0100

    Version 0.0.27
    - Fixed iron golem extra stats
    - Added initial spawnmob command
    - Last planned features before 1.0.0 are now done, only (possible) bugfixes left before upload

--- src/com/magmaguy/magmasmobs/CommandHandler.java
+++ src/com/magmaguy/magmasmobs/CommandHandler.java
@@ -13,30 +13,46 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.magmaguy.magmasmobs;
 
+import com.magmaguy.magmasmobs.mobcustomizer.HealthHandler;
+import com.magmaguy.magmasmobs.mobcustomizer.NameHandler;
 import com.magmaguy.magmasmobs.superdrops.SuperDropsHandler;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 import static com.magmaguy.magmasmobs.MagmasMobs.worldList;
 import static com.magmaguy.magmasmobs.superdrops.SuperDropsHandler.lootList;
-import static org.bukkit.Bukkit.getLogger;
-import static org.bukkit.Bukkit.getServer;
+import static org.bukkit.Bukkit.*;
 
 /**
  * Created by MagmaGuy on 21/01/2017.
@@ -54,6 +70,26 @@ public class CommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+
+        // /magmasmobs spawnmob with variable arg length
+        if (args.length > 0 && args[0].equalsIgnoreCase("spawnmob")) {
+
+            if (commandSender instanceof ConsoleCommandSender || commandSender instanceof Player && commandSender.hasPermission("magmasmobs.spawnmob")) {
+
+                spawnmob(commandSender, args);
+
+                return true;
+
+            } else if (commandSender instanceof Player && !commandSender.hasPermission("magmasmobs.spawnmob")) {
+
+                Player player = (Player) commandSender;
+                player.sendTitle("I'm afraid I can't let you do that, " + player.getDisplayName() + ".",
+                        "You need the following permission: " + "magmasmobs.spawnmob");
+                return true;
+
+            }
+
+        }
 
         switch(args.length) {
 
@@ -257,6 +293,221 @@ public class CommandHandler implements CommandExecutor {
 
     }
 
+    private void spawnmob(CommandSender commandSender, String[] args) {
+
+        World world = null;
+        Location location = null;
+        String entityInput = null;
+        int mobLevel = 0;
+        List<String> mobPower = null;
+
+        if (commandSender instanceof Player) {
+
+            Player player = (Player) commandSender;
+
+            if (args.length == 1) {
+
+                player.sendMessage("Valid command syntax:");
+                player.sendMessage("/magmasmobs spawnmob [mobType] [mobLevel] [mobPower] [mobPower(you can keep adding these mobPowers as many as you'd like)]");
+
+            }
+
+            world = player.getWorld();
+            location = player.getTargetBlock((HashSet<Byte>) null, 30).getLocation().add(0, 1, 0);
+
+            entityInput = args[1].toLowerCase();
+
+            if (args.length > 2) {
+
+                try {
+
+                    mobLevel = Integer.valueOf(args[2]);
+
+                } catch (NumberFormatException ex) {
+
+                    player.sendMessage("Not a valid level.");
+                    player.sendMessage("Valid command syntax:");
+                    player.sendMessage("/magmasmobs spawnmob [mobType] [mobLevel] [mobPower] [mobPower(you can keep adding these mobPowers as many as you'd like)]");
+
+                }
+
+            }
+
+            if (args.length > 3) {
+
+                int index = 0;
+
+                for (String arg : args) {
+
+                    //mob powers start after arg 2
+                    if (index > 2) {
+
+                        mobPower.add(arg);
+
+                    }
+
+                    index++;
+
+                }
+
+            }
+
+        } else if (commandSender instanceof ConsoleCommandSender) {
+
+            for (World worldIterator : worldList) {
+
+                //find world
+                if (worldIterator.getName().equals(args[1])) {
+
+                    world = worldIterator;
+
+                    //find x coord
+                    try {
+
+                        double xCoord = Double.parseDouble(args[2]);
+                        double yCoord = Double.parseDouble(args[3]);
+                        double zCoord = Double.parseDouble(args[4]);
+
+                        location = new Location(worldIterator, xCoord, yCoord, zCoord);
+
+                        entityInput = args[5].toLowerCase();
+
+                        break;
+
+                    } catch (NumberFormatException ex) {
+
+                        getConsoleSender().sendMessage("At least one of the coordinates (x:" + args[2] + ", y:" +
+                                args[3] + ", z:" + args[4] + ") is not valid");
+                        getConsoleSender().sendMessage("Valid command syntax: /magmasmobs spawnmob worldName xCoord yCoord " +
+                                "zCoord mobLevel mobPower mobPower(you can keep adding these mobPowers as many as you'd like)");
+
+                    }
+
+                }
+
+            }
+
+            if (world == null) {
+
+                getConsoleSender().sendMessage("World " + args[1] + "not found. Valid command syntax: /magmasmobs spawnmob" +
+                        " [worldName] [xCoord] [yCoord] [zCoord] [mobType] [mobLevel] [mobPower] [mobPower(you can keep adding these " +
+                        "mobPowers as many as you'd like)]");
+
+            }
+
+        }
+
+        EntityType entityType = null;
+
+        switch (entityInput) {
+
+            case "blaze":
+                entityType = EntityType.BLAZE;
+                break;
+            case "cavespider":
+                entityType = EntityType.CAVE_SPIDER;
+                break;
+            case "creeper":
+                entityType = EntityType.CREEPER;
+                break;
+            case "enderman":
+                entityType = EntityType.ENDERMAN;
+                break;
+            case "endermite":
+                entityType = EntityType.ENDERMITE;
+                break;
+            case "husk":
+                entityType = EntityType.HUSK;
+                break;
+            case "irongolem":
+                entityType = EntityType.IRON_GOLEM;
+                break;
+            case "pigzombie":
+                entityType = EntityType.PIG_ZOMBIE;
+                break;
+            case "polarbear":
+                entityType = EntityType.POLAR_BEAR;
+                break;
+            case "silverfish":
+                entityType = EntityType.SILVERFISH;
+                break;
+            case "skeleton":
+                entityType = EntityType.SKELETON;
+                break;
+            case "spider":
+                entityType = EntityType.SPIDER;
+                break;
+            case "stray":
+                entityType = EntityType.STRAY;
+                break;
+            case "witch":
+                entityType = EntityType.WITCH;
+                break;
+            case "witherskeleton":
+                entityType = EntityType.WITHER_SKELETON;
+                break;
+            case "zombie":
+                entityType = EntityType.ZOMBIE;
+                break;
+            case "chicken":
+                entityType = EntityType.CHICKEN;
+                break;
+            case "cow":
+                entityType = EntityType.COW;
+                break;
+            case "mushroomcow":
+                entityType = EntityType.MUSHROOM_COW;
+                break;
+            case "pig":
+                entityType = EntityType.PIG;
+                break;
+            case "sheep":
+                entityType = EntityType.SHEEP;
+                break;
+            default:
+                if (commandSender instanceof Player) {
+
+                    ((Player) commandSender).getPlayer().sendTitle("Could not spawn mob type " + entityInput,
+                            "If this is incorrect, please contact the dev.");
+
+                } else if (commandSender instanceof ConsoleCommandSender) {
+
+                    getConsoleSender().sendMessage("Could not spawn mob type " + entityInput + ". If this is incorrect, " +
+                            "please contact the dev.");
+
+                }
+                break;
+
+        }
+
+        Entity entity = null;
+
+        if (entityType != null) {
+
+            entity = world.spawnEntity(location, entityType);
+
+        }
+
+        if (entityType == EntityType.CHICKEN || entityType == EntityType.COW || entityType == EntityType.MUSHROOM_COW ||
+                entityType == EntityType.PIG || entityType == EntityType.SHEEP) {
+
+            HealthHandler.passiveHealthHandler(entity, plugin.getConfig().getInt("Passive SuperMob stack amount"));
+            NameHandler.customPassiveName(entity, plugin);
+
+            return;
+
+        }
+
+        if (mobLevel > 0) {
+
+            entity.setMetadata("MagmasSuperMob", new FixedMetadataValue(plugin, mobLevel));
+
+        }
+
+        //todo: add a way to select the powers mobs get, get alt system to spawn unstackable supermobs with fixed powers
+
+    }
+
     private void validCommands(CommandSender commandSender){
 
         if (commandSender instanceof Player) {
@@ -270,7 +521,7 @@ public class CommandHandler implements CommandExecutor {
             player.sendMessage("/magmasmobs getloot [loot name]");
             player.sendMessage("/magmasmobs giveloot [player name] random/[loot_name_underscore_for_spaces]");
 
-        }else if (commandSender instanceof ConsoleCommandSender) {
+        } else if (commandSender instanceof ConsoleCommandSender) {
 
             getLogger().info("Command not recognized. Valid commands:");
             getLogger().info("magmasmobs stats");
