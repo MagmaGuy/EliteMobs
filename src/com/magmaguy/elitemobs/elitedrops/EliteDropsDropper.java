e6e1ce51d2a7d71331e9908bd96cfc19d29c42ca
MagmaGuy <tiagoarnaut@gmail.com>
2017-06-07 05:36:00 +0100

    Version 2.4.10
    - First functional random loot system, still using debug names

--- src/com/magmaguy/elitemobs/elitedrops/EliteDropsDropper.java
+++ src/com/magmaguy/elitemobs/elitedrops/EliteDropsDropper.java
@@ -22,6 +22,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import java.util.Random;
@@ -33,6 +34,8 @@ public class EliteDropsDropper implements Listener{
 
     Plugin plugin = Bukkit.getPluginManager().getPlugin(MetadataHandler.ELITE_MOBS);
 
+    private Random random = new Random();
+
     @EventHandler
     public void onDeath(EntityDeathEvent event) {
 
@@ -41,8 +44,70 @@ public class EliteDropsDropper implements Listener{
         Random random = new Random();
 
         if (entity.hasMetadata(MetadataHandler.NATURAL_MOB_MD) &&
-                entity.hasMetadata(MetadataHandler.ELITE_MOB_MD) &&
-                entity.getMetadata(MetadataHandler.ELITE_MOB_MD).get(0).asInt() > 4) {
+                entity.hasMetadata(MetadataHandler.ELITE_MOB_MD)) {
+
+            if (!EliteDropsHandler.rankedItemStacks.isEmpty()){
+
+                //flat drop rate
+                if (random.nextDouble() < ConfigValues.defaultConfig.getDouble("Aggressive EliteMobs flat loot drop rate %") / 100) {
+
+                    //dropped item rank should be 0.4x the mob level (level 100 = rank 40 item)
+                    int rankRandomizer = (int) ((entity.getMetadata(MetadataHandler.ELITE_MOB_MD).get(0).asInt() * 0.5) + (random.nextInt(2) + 1 - 4));
+
+                    if (rankRandomizer < 1) {
+
+                        rankRandomizer = 0;
+
+                    }
+
+                    if (EliteDropsHandler.rankedItemStacks.containsKey(rankRandomizer)) {
+
+                        //chance of custom item dropping goes from 0% to config-based each custom item in that tier adds 1% chance
+                        if (random.nextDouble() < ConfigValues.randomItemsConfig.getDouble("Percentage (%) of times random item drop instead of custom loot (assuming 50 items in that tier)")) {
+
+                            //create random loot
+                            RandomItemGenerator randomItemGenerator = new RandomItemGenerator();
+                            ItemStack randomLoot = randomItemGenerator.randomItemGenerator(rankRandomizer);
+
+                            entity.getWorld().dropItem(entity.getLocation(), randomLoot);
+
+                        } else {
+
+                            //drop custom loot
+                            int randomCustomDrop = random.nextInt(EliteDropsHandler.rankedItemStacks.get(rankRandomizer).size());
+
+                            //get rank matching randomizer and item matching randomized index
+                            entity.getWorld().dropItem(entity.getLocation(), EliteDropsHandler.rankedItemStacks.get(rankRandomizer).get(randomCustomDrop));
+
+                            RandomItemGenerator randomItemGenerator = new RandomItemGenerator();
+                            ItemStack randomLoot = randomItemGenerator.randomItemGenerator(rankRandomizer);
+                            entity.getWorld().dropItem(entity.getLocation(), randomLoot);
+
+                        }
+
+                    }
+
+                    RandomItemGenerator randomItemGenerator = new RandomItemGenerator();
+                    ItemStack randomLoot = randomItemGenerator.randomItemGenerator(rankRandomizer);
+                    entity.getWorld().dropItem(entity.getLocation(), randomLoot);
+
+                }
+
+                //double drops
+                if (ConfigValues.defaultConfig.getBoolean("Aggressive EliteMobs can drop additional loot with drop % based on EliteMob level (higher is more likely)") &&
+                        random.nextDouble() < (entity.getMetadata(MetadataHandler.ELITE_MOB_MD).get(0).asInt() / 100)) {
+
+                    int randomDrop = random.nextInt(EliteDropsHandler.lootList.size());
+
+                    entity.getWorld().dropItem(entity.getLocation(), EliteDropsHandler.lootList.get(randomDrop));
+
+                }
+
+            } else {
+
+                //todo: add pure random gen item loot
+
+            }
 
 
             if (random.nextDouble() < ConfigValues.defaultConfig.getDouble("Aggressive EliteMobs flat loot drop rate %") / 100) {
--- src/com/magmaguy/elitemobs/elitedrops/RandomItemGenerator.java
+++ src/com/magmaguy/elitemobs/elitedrops/RandomItemGenerator.java
@@ -15,6 +15,16 @@
 
 package com.magmaguy.elitemobs.elitedrops;
 
+import com.magmaguy.elitemobs.config.ConfigValues;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -22,53 +32,309 @@ import java.util.Random;
  */
 public class RandomItemGenerator {
 
-    Random random = new Random();
+    private Random random = new Random();
 
-//    public ItemStack randomItemGenerator(int rankLevel) {
-//
-//        ItemStack randomItem = new ItemStack(randomMaterialConstructor(), 1);
-//
-//    }
-//
-//    private Material randomMaterialConstructor(){
-//
-//        List<Material> validMaterials = new ArrayList<>();
-//
-//        for (Object object : ConfigValues.randomItemsConfig.getList("Valid material list for random items")) {
-//
-//            try {
-//
-//                Material parsedMaterial = Material.getMaterial(object.toString());
-//                validMaterials.add(parsedMaterial);
-//
-//            } catch (Exception e) {
-//
-//                Bukkit.getLogger().info("Invalid material type detected: " + object.toString());
-//
-//            }
-//
-//
-//        }
-//
-//        validMaterials.addAll((Collection<? extends Material>) ConfigValues.randomItemsConfig.getList("Valid material list for random items"));
-//
-//        int index = random.nextInt(validMaterials.size()) + 1;
-//
-//        Material material = validMaterials.get(index);
-//
-//    }
-//
-//    private String randomItemNameConstructor(){
-//
-//    }
-//
-//    private List<String> randomItemLoreConstructor(){
-//
-//
-//    }
-//
-//    private void randomItemEnchantmentConstructor(){
-//
-//    }
+    public ItemStack randomItemGenerator(int rankLevel) {
+
+        //Create itemstack, generate material
+        ItemStack randomItem = new ItemStack(randomMaterialConstructor(), 1);
+        ItemMeta itemMeta = randomItem.getItemMeta();
+
+        //Apply item name
+        itemMeta.setDisplayName(randomItemNameConstructor());
+
+
+        //Apply lore
+        itemMeta.setLore(randomItemLoreConstructor(rankLevel));
+
+        //Apply enchantments
+        itemMeta = randomItemEnchantmentConstructor(randomItem.getType(), itemMeta, rankLevel);
+
+        randomItem.setItemMeta(itemMeta);
+
+        Bukkit.getLogger().info("item randomized");
+
+        return randomItem;
+
+    }
+
+    private Material randomMaterialConstructor(){
+
+        List<Material> validMaterials = new ArrayList<>();
+
+        for (Object object : ConfigValues.randomItemsConfig.getList("Valid material list for random items")) {
+
+            try {
+
+                Material parsedMaterial = Material.getMaterial(object.toString());
+                validMaterials.add(parsedMaterial);
+
+            } catch (Exception e) {
+
+                Bukkit.getLogger().info("Invalid material type detected: " + object.toString());
+
+            }
+
+
+        }
+
+        int index = random.nextInt(validMaterials.size()) + 1;
+
+        Material material = validMaterials.get(index);
+
+        return material;
+
+    }
+
+    private String randomItemNameConstructor(){
+
+        return "Butts";
+
+    }
+
+    private List<String> randomItemLoreConstructor(int rankLevel){
+
+        String lore = "Rank " + rankLevel + " Elite Mob Drop";
+
+        List<String> loreList = new ArrayList<>();
+
+        loreList.add(lore);
+
+        return loreList;
+
+    }
+
+    HashMap<Enchantment, Integer> validEnchantments = new HashMap();
+
+    private ItemMeta randomItemEnchantmentConstructor(Material material, ItemMeta oldMeta, int rankLevel){
+
+        if (material.equals(Material.DIAMOND_SWORD) || material.equals(Material.GOLD_SWORD) ||
+                material.equals(Material.IRON_SWORD) || material.equals(Material.STONE_SWORD) ||
+                material.equals(Material.WOOD_SWORD)) {
+
+            validateEnchantment("DAMAGE_ALL");
+            validateEnchantment("DAMAGE_ARTHROPODS");
+            validateEnchantment("DAMAGE_UNDEAD");
+            validateEnchantment("DURABILITY");
+            validateEnchantment("FIRE_ASPECT");
+            validateEnchantment("KNOCKBACK");
+            validateEnchantment("LOOT_BONUS_MOBS");
+            validateEnchantment("LUCK");
+            validateEnchantment("MENDING");
+            validateEnchantment("SWEEPING_EDGE");
+            validateEnchantment("VANISHING_CURSE");
+
+        } else if (material.equals(Material.BOW)) {
+
+            validateEnchantment("ARROW_DAMAGE");
+            validateEnchantment("ARROW_FIRE");
+            validateEnchantment("ARROW_INFINITE");
+            validateEnchantment("ARROW_KNOCKBACK");
+            validateEnchantment("DURABILITY");
+            validateEnchantment("LUCK");
+            validateEnchantment("MENDING");
+            validateEnchantment("VANISHING_CURSE");
+
+        } else if (material.equals(Material.DIAMOND_PICKAXE) || material.equals(Material.GOLD_PICKAXE) ||
+                material.equals(Material.IRON_PICKAXE) || material.equals(Material.STONE_PICKAXE) ||
+                material.equals(Material.WOOD_PICKAXE)) {
+
+            validateEnchantment("DURABILITY");
+            validateEnchantment("MENDING");
+            validateEnchantment("VANISHING_CURSE");
+            validateEnchantment("DIG_SPEED");
+            validateEnchantment("LOOT_BONUS_BLOCKS");
+            validateEnchantment("SILK_TOUCH");
+
+        } else if (material.equals(Material.DIAMOND_SPADE) || material.equals(Material.GOLD_SPADE) ||
+                material.equals(Material.IRON_SPADE) || material.equals(Material.STONE_SPADE) ||
+                material.equals(Material.WOOD_SPADE)){
+
+            validateEnchantment("DURABILITY");
+            validateEnchantment("MENDING");
+            validateEnchantment("VANISHING_CURSE");
+            validateEnchantment("DIG_SPEED");
+            validateEnchantment("LOOT_BONUS_BLOCKS");
+            validateEnchantment("SILK_TOUCH");
+
+        } else if (material.equals(Material.DIAMOND_HOE) || material.equals(Material.GOLD_HOE) ||
+                material.equals(Material.IRON_HOE) || material.equals(Material.STONE_HOE) ||
+                material.equals(Material.WOOD_HOE)) {
+
+            validateEnchantment("DURABILITY");
+            validateEnchantment("MENDING");
+            validateEnchantment("VANISHING_CURSE");
+
+        } else if (material.equals(Material.DIAMOND_AXE) || material.equals(Material.GOLD_AXE) ||
+                material.equals(Material.IRON_AXE) || material.equals(Material.STONE_AXE) ||
+                material.equals(Material.WOOD_AXE)) {
+
+            validateEnchantment("DAMAGE_ALL");
+            validateEnchantment("DAMAGE_ARTHROPODS");
+            validateEnchantment("DAMAGE_UNDEAD");
+            validateEnchantment("DURABILITY");
+            validateEnchantment("FIRE_ASPECT");
+            validateEnchantment("KNOCKBACK");
+            validateEnchantment("LOOT_BONUS_MOBS");
+            validateEnchantment("LUCK");
+            validateEnchantment("MENDING");
+            validateEnchantment("VANISHING_CURSE");
+            validateEnchantment("DIG_SPEED");
+            validateEnchantment("LOOT_BONUS_BLOCKS");
+
+        } else if (material.equals(Material.CHAINMAIL_HELMET) || material.equals(Material.DIAMOND_HELMET) ||
+                material.equals(Material.GOLD_HELMET) || material.equals(Material.IRON_HELMET) ||
+                material.equals(Material.LEATHER_HELMET)) {
+
+            validateEnchantment("BINDING_CURSE");
+            validateEnchantment("DURABILITY");
+            validateEnchantment("MENDING");
+            validateEnchantment("OXYGEN");
+            validateEnchantment("PROTECTION_ENVIRONMENTAL");
+            validateEnchantment("PROTECTION_EXPLOSIONS");
+            validateEnchantment("PROTECTION_FALL");
+            validateEnchantment("PROTECTION_FIRE");
+            validateEnchantment("PROTECTION_PROJECTILE");
+            validateEnchantment("THORNS");
+            validateEnchantment("VANISHING_CURSE");
+            validateEnchantment("WATER_WORKER");
+
+        } else if (material.equals(Material.CHAINMAIL_CHESTPLATE) || material.equals(Material.DIAMOND_CHESTPLATE) ||
+                material.equals(Material.GOLD_CHESTPLATE) || material.equals(Material.IRON_CHESTPLATE) ||
+                material.equals(Material.LEATHER_CHESTPLATE)) {
+
+            validateEnchantment("BINDING_CURSE");
+            validateEnchantment("DURABILITY");
+            validateEnchantment("MENDING");
+            validateEnchantment("OXYGEN");
+            validateEnchantment("PROTECTION_ENVIRONMENTAL");
+            validateEnchantment("PROTECTION_EXPLOSIONS");
+            validateEnchantment("PROTECTION_FALL");
+            validateEnchantment("PROTECTION_FIRE");
+            validateEnchantment("PROTECTION_PROJECTILE");
+            validateEnchantment("THORNS");
+            validateEnchantment("VANISHING_CURSE");
+            validateEnchantment("WATER_WORKER");
+
+        } else if (material.equals(Material.CHAINMAIL_LEGGINGS) || material.equals(Material.DIAMOND_LEGGINGS) ||
+                material.equals(Material.GOLD_LEGGINGS) || material.equals(Material.IRON_LEGGINGS) ||
+                material.equals(Material.LEATHER_LEGGINGS)) {
+
+            validateEnchantment("BINDING_CURSE");
+            validateEnchantment("DURABILITY");
+            validateEnchantment("MENDING");
+            validateEnchantment("OXYGEN");
+            validateEnchantment("PROTECTION_ENVIRONMENTAL");
+            validateEnchantment("PROTECTION_EXPLOSIONS");
+            validateEnchantment("PROTECTION_FALL");
+            validateEnchantment("PROTECTION_FIRE");
+            validateEnchantment("PROTECTION_PROJECTILE");
+            validateEnchantment("THORNS");
+            validateEnchantment("VANISHING_CURSE");
+            validateEnchantment("WATER_WORKER");
+
+        } else if (material.equals(Material.CHAINMAIL_BOOTS) || material.equals(Material.DIAMOND_BOOTS) ||
+                material.equals(Material.GOLD_BOOTS) || material.equals(Material.IRON_BOOTS) ||
+                material.equals(Material.LEATHER_BOOTS)) {
+
+            validateEnchantment("BINDING_CURSE");
+            validateEnchantment("DURABILITY");
+            validateEnchantment("MENDING");
+            validateEnchantment("OXYGEN");
+            validateEnchantment("PROTECTION_ENVIRONMENTAL");
+            validateEnchantment("PROTECTION_EXPLOSIONS");
+            validateEnchantment("PROTECTION_FALL");
+            validateEnchantment("PROTECTION_FIRE");
+            validateEnchantment("PROTECTION_PROJECTILE");
+            validateEnchantment("THORNS");
+            validateEnchantment("VANISHING_CURSE");
+            validateEnchantment("WATER_WORKER");
+
+        }
+
+        if (validEnchantments.size() == 0) {
+
+            return oldMeta;
+
+        }
+
+        ItemMeta newMeta = oldMeta;
+
+        HashMap<Enchantment, Integer> validEnchantmentsClone = (HashMap<Enchantment, Integer>) validEnchantments.clone();
+
+        int materialRank = ItemRankHandler.itemTypePower(material);
+        rankLevel -= materialRank;
+
+        //randomizer for enchantments
+        for (int i = 0; i < rankLevel; i++) {
+
+            int randomIndex = random.nextInt(validEnchantments.size());
+
+            List<Enchantment> enchantmentList = new ArrayList();
+
+            for (Enchantment enchantment : validEnchantments.keySet()) {
+
+                enchantmentList.add(enchantment);
+
+            }
+
+            String enchantmentString = enchantmentList.get(randomIndex).getName();
+
+            Enchantment enchantment = enchantmentList.get(randomIndex);
+
+            validEnchantments.put(enchantment, validEnchantments.get(enchantment) - 1);
+
+            if (ConfigValues.randomItemsConfig.contains("Valid Enchantments." + enchantmentString + ".Max Level")) {
+
+                int finalEnchantLevel = validEnchantmentsClone.get(enchantment) - validEnchantments.get(enchantment);
+
+                newMeta.addEnchant(enchantment, finalEnchantLevel, true);
+
+                Bukkit.getLogger().info(validEnchantmentsClone.get(enchantment) + "");
+
+            } else {
+
+                newMeta.addEnchant(enchantment, 1, true);
+
+            }
+
+            int newEnchantInt = validEnchantments.get(enchantment);
+
+            if (newEnchantInt == 0) {
+
+                validEnchantments.remove(enchantment);
+
+            }
+
+        }
+
+        return newMeta;
+
+    }
+
+    private void validateEnchantment(String string) {
+
+        String mainString = "Valid Enchantments." + string;
+
+        if (ConfigValues.randomItemsConfig.getBoolean(mainString + ".Allow")) {
+
+            int enchantmentLevel;
+
+            if (ConfigValues.randomItemsConfig.contains(mainString + ".Max Level")) {
+
+                enchantmentLevel = ConfigValues.randomItemsConfig.getInt(mainString + ".Max Level");
+
+            } else {
+
+                enchantmentLevel = 1;
+
+            }
+
+            //index for random getting
+            validEnchantments.put(Enchantment.getByName(string), enchantmentLevel);
+
+        }
+
+    }
 
 }
