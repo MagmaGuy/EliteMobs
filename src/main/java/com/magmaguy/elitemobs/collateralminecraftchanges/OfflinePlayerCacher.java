ef35dccfab5c0e3d5195ff375335fc9041532a06
MagmaGuy <tiagoarnaut@gmail.com>
2017-07-02 21:09:15 +0100

    Version 4.0.0
    - Cleaned up some debug code
    - Public release of 3.0.x features

--- src/main/java/com/magmaguy/elitemobs/collateralminecraftchanges/OfflinePlayerCacher.java
+++ src/main/java/com/magmaguy/elitemobs/collateralminecraftchanges/OfflinePlayerCacher.java
@@ -16,7 +16,6 @@
 package com.magmaguy.elitemobs.collateralminecraftchanges;
 
 import com.magmaguy.elitemobs.config.PlayerCacheConfig;
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -39,14 +38,12 @@ public class OfflinePlayerCacher implements Listener {
         if (!configuration.contains(UUID)) {
 
             configuration.set(UUID, playerName);
-            Bukkit.getLogger().info("test1");
 
             playerCacheConfig.saveCustomConfig();
 
         } else if (!configuration.getString(UUID).equals(playerName)) {
 
             configuration.set(UUID, playerName);
-            Bukkit.getLogger().info("test2");
 
             playerCacheConfig.saveCustomConfig();
 
