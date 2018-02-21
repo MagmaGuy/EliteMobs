284eab98a242bae5c95c4c9529654dc0f4012985
MagmaGuy <tiagoarnaut@gmail.com>
2018-02-21 02:25:23 +0000

    Version 5.2.0
    - Fixed issue with events not launching
    - Removed deprecated "Percentage (%) of times random item drop instead of custom loot" option
    -

--- src/main/java/com/magmaguy/elitemobs/naturalmobspawner/NaturalMobSpawner.java
+++ src/main/java/com/magmaguy/elitemobs/naturalmobspawner/NaturalMobSpawner.java
@@ -56,8 +56,8 @@ public class NaturalMobSpawner implements Listener {
 
             if (player.getWorld().equals(entity.getWorld()) && player.getLocation().distance(entity.getLocation()) < range) {
 
-                if (!player.getGameMode().equals(GameMode.SPECTATOR) && (!player.hasMetadata("vanished") ||
-                        player.hasMetadata("vanished") && !player.getMetadata("vanished").get(0).asBoolean())) {
+                if (!player.getGameMode().equals(GameMode.SPECTATOR) && (!player.hasMetadata(MetadataHandler.VANISH_NO_PACKET) ||
+                        player.hasMetadata(MetadataHandler.VANISH_NO_PACKET) && !player.getMetadata(MetadataHandler.VANISH_NO_PACKET).get(0).asBoolean())) {
 
                     closePlayers.add(player);
 
