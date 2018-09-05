60719a4fead57c577a9200b6864ee40c2bfe1abf
MagmaGuy <tiagoarnaut@gmail.com>
2018-09-05 18:01:15 +0100

    Version 6.6.0
    - Public release of 6.5.X changes

---
+++ src/main/java/com/magmaguy/elitemobs/utils/VersionChecker.java
@@ -0,0 +1,41 @@
+package com.magmaguy.elitemobs.utils;
+
+import org.bukkit.Bukkit;
+
+public class VersionChecker {
+
+    public static boolean currentVersionIsUnder(int version, int subVersion) {
+
+        if (Bukkit.getBukkitVersion().split("[.]").length < 4)
+            return Integer.parseInt(Bukkit.getBukkitVersion().split("[.]")[1].substring(0, 2)) < version;
+
+        if (Integer.parseInt(Bukkit.getBukkitVersion().split("[.]")[1]) < version) return true;
+
+        if (Integer.parseInt(Bukkit.getBukkitVersion().split("[.]")[1]) == version) {
+            try {
+
+                Byte actualSubversion = Byte.parseByte(Bukkit.getBukkitVersion().split("[.]")[2].substring(0, 1));
+                return actualSubversion < subVersion;
+
+            } catch (Exception e) {
+
+                try {
+
+                    Byte actualSubversion = Byte.parseByte(Bukkit.getBukkitVersion().split("[.]")[2].substring(0, 0));
+                    return actualSubversion < subVersion;
+
+                } catch (Exception ex) {
+
+                    Bukkit.getLogger().warning("[EliteMobs] Couldn't read version properly, report this to the developer!");
+                    return false;
+
+                }
+            }
+
+        }
+
+        return false;
+
+    }
+
+}
