72c2fce90a446df1c5664adbe9f167ea13abf191
MagmaGuy <tiagoarnaut+github@gmail.com>
2018-10-12 12:55:59 +0100

    Version 6.6.2.7 (non-compile version)
    - More tweaks to the custom enchantments system

--- src/main/java/com/magmaguy/elitemobs/items/customenchantments/CustomEnchantment.java
+++ src/main/java/com/magmaguy/elitemobs/items/customenchantments/CustomEnchantment.java
@@ -2,12 +2,43 @@ package com.magmaguy.elitemobs.items.customenchantments;
 
 public abstract class CustomEnchantment {
 
-    public String name = setName();
+    /*
+    Code here has been maintained as static because the key value is frequently accessed and stores a static value
+    setKey() and setName() need to be rewritten in every class
+     */
 
-    public abstract String setName();
+    /*
+    Key is used to hide the enchantment in the lore to later ID the item, is independent from name to avoid issues with
+    config changes invalidating old enchantment names
+     */
+    public static String key = setKey();
 
-    public String getName(){
+    public static String setKey() {
+        return "placeholder key";
+    }
+
+    public static String getKey() {
+        return key;
+    }
+
+    /*
+    Name is taken from config settings
+     */
+    public static String name = setName();
+
+    public static String setName() {
+        return "placeholder name";
+    }
+
+    public static String getName() {
         return name;
+    }
+
+    /*
+    Get the enchantment string for assembling configurations
+     */
+    public static String assembleConfigString(int enchantmentLevel){
+        return key + "," + enchantmentLevel;
     }
 
 }
