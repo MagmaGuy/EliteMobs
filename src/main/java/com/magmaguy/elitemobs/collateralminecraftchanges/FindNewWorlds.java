9f4d61921f68e6275d7a77d2c96524cf652b50f5
MagmaGuy <tiagoarnaut@gmail.com>
2019-04-13 05:38:36 +0100

    Version 7.0.0
    - Public release of developer version features
    - Added dynamic world loading support for npc spawning

--- src/main/java/com/magmaguy/elitemobs/collateralminecraftchanges/FindNewWorlds.java
+++ src/main/java/com/magmaguy/elitemobs/collateralminecraftchanges/FindNewWorlds.java
@@ -1,7 +1,9 @@
 package com.magmaguy.elitemobs.collateralminecraftchanges;
 
+import com.magmaguy.elitemobs.config.AdventurersGuildConfig;
 import com.magmaguy.elitemobs.config.ConfigValues;
 import com.magmaguy.elitemobs.config.ValidWorldsConfig;
+import com.magmaguy.elitemobs.npcs.NPCInitializer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldLoadEvent;
@@ -14,6 +16,10 @@ public class FindNewWorlds implements Listener {
         ValidWorldsConfig validWorldsConfig = new ValidWorldsConfig();
         validWorldsConfig.initializeConfig();
         ConfigValues.validWorldsConfig = validWorldsConfig.configuration;
+
+        if (event.getWorld().getName().equals(ConfigValues.adventurersGuildConfig.getString(AdventurersGuildConfig.GUILD_WORLD_NAME))) {
+            NPCInitializer npcInitializer = new NPCInitializer();
+        }
 
     }
 
