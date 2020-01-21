f24b74bb3c9e2d0eb7bd2c7ff905bb99f8053f33
MagmaGuy <tiagoarnaut@gmail.com>
2020-01-21 20:45:26 +0000

    Version 7.1.3 - Added the following placeholders:     - %elitemobs_player_combat_tier%     - %elitemobs_player_active_guild_rank_numerical%     - %elitemobs_player_maximum_guild_rank_numerical%     - %elitemobs_player_active_guild_rank_name%     - %elitemobs_player_maximum_guild_rank_name%     - %elitemobs_player_money%     - %elitemobs_player_top_tier%     - %elitemobs_player_top_guild_rank% - Fixed Adventurer's Guild world sometimes being unavailable after startup - Added simloot command for console [/em simloot {playername} {tier}] - Tentative task error spam fix - Minor compatibility fixes
    Signed-off-by: MagmaGuy <tiagoarnaut@gmail.com>

--- src/main/java/com/magmaguy/elitemobs/commands/SimLootHandler.java
+++ src/main/java/com/magmaguy/elitemobs/commands/SimLootHandler.java
@@ -1,21 +1,8 @@
-/*
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.magmaguy.elitemobs.commands;
 
 import com.magmaguy.elitemobs.items.itemconstructor.ItemConstructor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -23,6 +10,33 @@ import org.bukkit.inventory.ItemStack;
  * Created by MagmaGuy on 08/06/2017.
  */
 public class SimLootHandler {
+
+    public static void simLoot(CommandSender commandSender, String[] args) {
+        if (args.length < 3) {
+            commandSender.sendMessage("[EM] Not enough arguments! Correct syntax:");
+            commandSender.sendMessage("- /elitemobs simloot [playername] [tier]");
+        }
+
+        Player player;
+
+        try {
+            player = Bukkit.getPlayer(args[1]);
+        } catch (Exception ex) {
+            commandSender.sendMessage("[EM]" + args[1] + " is not a valid player name!");
+            return;
+        }
+
+        int tier = 0;
+
+        try {
+            tier = Integer.parseInt(args[2]);
+        } catch (Exception ex) {
+            commandSender.sendMessage("[EM]" + args[2] + " is not a valid integer value!");
+        }
+
+        simLoot(player, tier);
+
+    }
 
     public static void simLoot(Player player, int level) {
         ItemStack itemStack = ItemConstructor.constructItem(level, null);
