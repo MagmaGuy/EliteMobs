304454d85b9d9ae7d6bf37fad67f6f9b76c841f3
MagmaGuy <tiagoarnaut@gmail.com>
2020-03-06 14:25:06 +0000

    Version 7.1.8 - Fixed bug introduced by spigot update where items wouldn't be recognized anymore, but this resets the existing gear progression
    Signed-off-by: MagmaGuy <tiagoarnaut@gmail.com>

--- src/main/java/com/magmaguy/elitemobs/items/ItemTagger.java
+++ src/main/java/com/magmaguy/elitemobs/items/ItemTagger.java
@@ -7,7 +7,7 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.tags.ItemTagType;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,13 +17,15 @@ public class ItemTagger {
     public static final NamespacedKey eliteMobsItemNamespacedKey = new NamespacedKey(MetadataHandler.PLUGIN, "EliteMobsItem");
 
     public static void registerEliteItem(ItemStack itemStack) {
-        itemStack.getItemMeta().getCustomTagContainer().setCustomTag(eliteMobsItemNamespacedKey, ItemTagType.BYTE, (byte) 1);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.getPersistentDataContainer().set(eliteMobsItemNamespacedKey, PersistentDataType.BYTE, (byte) 1);
+        itemStack.setItemMeta(itemMeta);
     }
 
     public static boolean isEliteItem(ItemStack itemStack) {
         if (itemStack == null) return false;
         if (!itemStack.hasItemMeta()) return false;
-        return itemStack.getItemMeta().getCustomTagContainer().hasCustomTag(eliteMobsItemNamespacedKey, ItemTagType.BYTE);
+        return itemStack.getItemMeta().getPersistentDataContainer().has(eliteMobsItemNamespacedKey, PersistentDataType.BYTE);
     }
 
     /**
@@ -35,7 +37,7 @@ public class ItemTagger {
     public static void registerEnchantments(ItemMeta itemMeta, HashMap<Enchantment, Integer> enchantmentMap) {
         for (Enchantment enchantment : enchantmentMap.keySet())
             if (EliteEnchantments.isPotentialEliteEnchantment(enchantment))
-                ItemTagger.registerEnchantment(itemMeta, enchantment.getKey(), enchantmentMap.get(enchantment));
+                registerEnchantment(itemMeta, enchantment.getKey(), enchantmentMap.get(enchantment));
     }
 
     /**
@@ -46,13 +48,13 @@ public class ItemTagger {
      * @param enchantmentLevel
      */
     public static void registerEnchantment(ItemMeta itemMeta, NamespacedKey enchantmentKey, int enchantmentLevel) {
-        itemMeta.getCustomTagContainer().setCustomTag(enchantmentKey, ItemTagType.INTEGER, enchantmentLevel);
+        itemMeta.getPersistentDataContainer().set(enchantmentKey, PersistentDataType.INTEGER, enchantmentLevel);
     }
 
 
     public static void registerCustomEnchantments(ItemMeta itemMeta, HashMap<String, Integer> customEnchantments) {
         for (String subString : customEnchantments.keySet())
-            ItemTagger.registerCustomEnchantment(itemMeta, subString, customEnchantments.get(subString));
+            registerCustomEnchantment(itemMeta, subString, customEnchantments.get(subString));
     }
 
     /**
@@ -63,13 +65,11 @@ public class ItemTagger {
      * @param enchantmentLevel
      */
     public static void registerCustomEnchantment(ItemMeta itemMeta, String enchantmentKey, int enchantmentLevel) {
-        itemMeta.getCustomTagContainer().setCustomTag(new NamespacedKey(MetadataHandler.PLUGIN, enchantmentKey),
-                ItemTagType.INTEGER, enchantmentLevel);
+        itemMeta.getPersistentDataContainer().set(new NamespacedKey(MetadataHandler.PLUGIN, enchantmentKey), PersistentDataType.INTEGER, enchantmentLevel);
     }
 
     public static void registerCustomEnchantment(ItemMeta itemMeta, String enchantmentKey, String uuid) {
-        itemMeta.getCustomTagContainer().setCustomTag(new NamespacedKey(MetadataHandler.PLUGIN, enchantmentKey),
-                ItemTagType.STRING, uuid);
+        itemMeta.getPersistentDataContainer().set(new NamespacedKey(MetadataHandler.PLUGIN, enchantmentKey), PersistentDataType.STRING, uuid);
     }
 
     /**
@@ -82,13 +82,13 @@ public class ItemTagger {
     public static int getEnchantment(ItemMeta itemMeta, NamespacedKey enchantmentKey) {
         if (itemMeta == null)
             return 0;
-        if (!itemMeta.getCustomTagContainer().hasCustomTag(enchantmentKey, ItemTagType.INTEGER))
+        if (!hasEnchantment(itemMeta, enchantmentKey))
             return 0;
-        return itemMeta.getCustomTagContainer().getCustomTag(enchantmentKey, ItemTagType.INTEGER);
+        return itemMeta.getPersistentDataContainer().get(enchantmentKey, PersistentDataType.INTEGER);
     }
 
     public static boolean hasEnchantment(ItemMeta itemMeta, NamespacedKey enchantmentKey) {
-        return itemMeta.getCustomTagContainer().hasCustomTag(enchantmentKey, ItemTagType.INTEGER);
+        return itemMeta.getPersistentDataContainer().has(enchantmentKey, PersistentDataType.INTEGER);
     }
 
     public static NamespacedKey onHitPotionEffectKey = new NamespacedKey(MetadataHandler.PLUGIN, "onHitPotionEffect");
