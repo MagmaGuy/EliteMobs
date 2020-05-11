28735ae95c4c379c1b19a0b716845684d7305f82
MagmaGuy <tiagoarnaut@gmail.com>
2020-05-11 11:48:08 +0100

    Version 7.1.23 - Fixed issue where custom bosses would drop vanilla loot when they weren't meant to - Removed redundant HideEnchantmentsAttributes config option - Added armor tier for turtle helmets - Fixed issue where armor stands wouldn't spawn for areas with the elitemobs-dungeon flag
    Signed-off-by: MagmaGuy <tiagoarnaut@gmail.com>

--- src/main/java/com/magmaguy/elitemobs/worldguard/WorldGuardDungeonFlag.java
+++ src/main/java/com/magmaguy/elitemobs/worldguard/WorldGuardDungeonFlag.java
@@ -5,6 +5,7 @@ import com.magmaguy.elitemobs.EntityTracker;
 import com.magmaguy.elitemobs.custombosses.CustomBossEntity;
 import com.magmaguy.elitemobs.mobconstructor.EliteMobEntity;
 import com.sk89q.worldguard.protection.flags.StateFlag;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,6 +16,7 @@ public class WorldGuardDungeonFlag implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onSpawn(CreatureSpawnEvent event) {
         if (!EliteMobs.worldguardIsEnabled) return;
+        if (event.getEntity().getType().equals(EntityType.ARMOR_STAND)) return;
         if (WorldGuardFlagChecker.checkFlag(event.getLocation(), (StateFlag) WorldGuardCompatibility.getEliteMobsDungeonFlag())) {
             EliteMobEntity eliteMobEntity = EntityTracker.getEliteMobEntity(event.getEntity());
             if (eliteMobEntity != null)
