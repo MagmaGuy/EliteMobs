d74693edefa40a8631706c3520d75a6d45ef7c57
MagmaGuy <tiagoarnaut@gmail.com>
2020-11-19 22:00:19 +0000

    Version 7.2.7 - [Hotfix] Elite Coins are now correctly soulbound - [Fix] All custom bosses are now able to escape instead of just regional bosses
    Signed-off-by: MagmaGuy <tiagoarnaut@gmail.com>

--- src/main/java/com/magmaguy/elitemobs/npcs/NPCWorkingHours.java
+++ src/main/java/com/magmaguy/elitemobs/npcs/NPCWorkingHours.java
@@ -2,6 +2,7 @@ package com.magmaguy.elitemobs.npcs;
 
 import com.magmaguy.elitemobs.ChatColorConverter;
 import com.magmaguy.elitemobs.MetadataHandler;
+import com.magmaguy.elitemobs.config.ConfigValues;
 import com.magmaguy.elitemobs.config.TranslationConfig;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -32,7 +33,7 @@ public class NPCWorkingHours {
                         npcEntity.getVillager().addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 12000, 1));
 
                         if (!npcEntity.getIsSleeping()) {
-                            npcEntity.setTempRole(ChatColorConverter.convert(TranslationConfig.NPC_SLEEPING_MESSAGE));
+                            npcEntity.setTempRole(ChatColorConverter.convert(ConfigValues.translationConfig.getString(TranslationConfig.NPC_SLEEPING_MESSAGE)));
                             npcEntity.setIsSleeping(true);
                         }
 
