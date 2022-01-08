6401798871591cf609d8921527f25010e42d802a
MagmaGuy <tiagoarnaut@gmail.com>
2022-01-08 02:41:04 +0000

    EliteMobs 7.3.12-SNAPSHOT-8 - [New] Added wormholes! A quick and easy way to teleport players from portal A to portal B and from portal B to portal A! - [Fix] Fixed error that would trigger when bosses tried to damage players after being dead, bosses can no longer do that - [Fix] Action events can no longer happen in disabled worlds - [Fix] Fixed bug which would cause on spawn commands to not run - [Refactor] Minor Sonarlint improvements
    Signed-off-by: MagmaGuy <tiagoarnaut@gmail.com>

---
+++ src/main/java/com/magmaguy/elitemobs/config/wormholes/WormholeConfigFields.java
@@ -0,0 +1,56 @@
+package com.magmaguy.elitemobs.config.wormholes;
+
+import com.magmaguy.elitemobs.config.CustomConfigFields;
+import com.magmaguy.elitemobs.config.CustomConfigFieldsInterface;
+import lombok.Getter;
+import lombok.Setter;
+import wormhole.Wormhole;
+
+public class WormholeConfigFields extends CustomConfigFields implements CustomConfigFieldsInterface {
+
+    @Getter
+    @Setter
+    private String location1;
+    @Getter
+    @Setter
+    private String location2;
+    @Getter
+    @Setter
+    private String permission;
+    @Getter
+    @Setter
+    private Wormhole.WormholeStyle style = Wormhole.WormholeStyle.CUBE;
+    @Getter
+    @Setter
+    private double coinCost;
+    @Getter
+    @Setter
+    private boolean blindPlayer;
+
+    public WormholeConfigFields(String filename,
+                                boolean isEnabled,
+                                String location1,
+                                String location2,
+                                Wormhole.WormholeStyle style) {
+        super(filename, isEnabled);
+        this.location1 = location1;
+        this.location2 = location2;
+        this.style = style;
+    }
+
+    public WormholeConfigFields(String filename, boolean isEnabled) {
+        super(filename, isEnabled);
+    }
+
+    @Override
+    public void processConfigFields() {
+        this.isEnabled = processBoolean("isEnabled", isEnabled, true, true);
+        this.location1 = processString("location1", location1, null, true);
+        this.location2 = processString("location2", location2, null, true);
+        this.permission = processString("permission", permission, null, false);
+        this.coinCost = processDouble("coinCost", coinCost, 0, false);
+        this.style = processEnum("style", style, Wormhole.WormholeStyle.CUBE, Wormhole.WormholeStyle.class, false);
+        this.blindPlayer = processBoolean("blindPlayer", blindPlayer, false, false);
+    }
+
+}
