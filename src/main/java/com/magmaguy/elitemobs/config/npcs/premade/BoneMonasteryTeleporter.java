d85b212b125bc688653ac2e8b658e67c05a653a8
MagmaGuy <tiagoarnaut@gmail.com>
2023-01-08 21:03:19 +0000

    EliteMobs 8.3.9 - [Fix] Arenas should now import correctly from dungeon packages
    Signed-off-by: MagmaGuy <tiagoarnaut@gmail.com>

--- src/main/java/com/magmaguy/elitemobs/config/npcs/premade/BoneMonasteryTeleporter.java
+++ src/main/java/com/magmaguy/elitemobs/config/npcs/premade/BoneMonasteryTeleporter.java
@@ -20,7 +20,7 @@ public class BoneMonasteryTeleporter extends NPCsConfigFields {
                 true,
                 1,
                 NPCInteractions.NPCInteractionType.COMMAND);
-        setCommand("em dungeontp the_bone_monastery.yml");
+        setCommand("em dungeontp bone_monastery_dungeon.yml");
         setDisguise("custom:elitemobs_bone_monastery_teleporter");
         setCustomDisguiseData("player elitemobs_bone_monastery_teleporter setskin {\"id\":\"a30519df-b8b1-42a8-a537-64d5ff8b5651\",\"name\":\"Unknown\",\"properties\":[{\"name\":\"textures\",\"value\":\"ewogICJ0aW1lc3RhbXAiIDogMTY0NzgwMjAxMzExNCwKICAicHJvZmlsZUlkIiA6ICJhYjlkYmMzZjk4NGE0ZWI4YTVmY2RlYWMzNzEzZWFkMSIsCiAgInByb2ZpbGVOYW1lIiA6ICJDeWJvcm51dDIiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTcwZjM0OThhYjRlMDhlMjIyYzI4OTBlYmMyMTU2ZjhhYjY1NmIwM2U3OGVhM2ZhMmViNmE0Y2ZjM2NiZjE1NSIsCiAgICAgICJtZXRhZGF0YSIgOiB7CiAgICAgICAgIm1vZGVsIiA6ICJzbGltIgogICAgICB9CiAgICB9CiAgfQp9\",\"signature\":\"KP+QjsBszTPBIGI/RU50q2aF9DhFFzLxqsYwxBlL8t+idG1AKYd4nt8LrDs4C8fKVmT+fHmCCJSxpakmZgQ1aHgPLN67A8Q614K3SR67vaseNMhVnmEmZ0clBFRHxjnVR134/ozeXFSGd2d8ttbZPzO8GxXWMa6ezbrWEPEsxl6n4l/Mj6v2zZQWkHBxhmgqxWh+z/708JBop0H5TDha+y2gikaRdRhurXXq67Ud/oghcMK6mTO5ET0m8/8TNz0uBF42rcMbT2Ew6t/h2SuW0ULvd3cb/gB4zUMjAXmhrtb0kHoC+gAPPv715TWG7LBnuOkAbl9SLCvMeSkSKUsKWTswb07DojrEl+kt8LZToebFlR37esNqSubPvFx7LJJZ+wBspTv+c9PNdfVSUMkzh02jKdgWEDFQv6WG26OZw+f9PPOztYRzmYEajezl0Re8G66aIa3G3eynWa+JMX3Ml9Jpym+P1wBkIo1nWlazJkumkPG6sVLqMOkvrCQlDedWoQacCDRVsUVnzBHrHkEoin5y+USkcxWwU4yhaPi7mX3SSG0mRiI3LpBurcCiJcQzlGszml2w5fJBLAeEUyO9UtUMxP/mS477FmbGeLvmP7+AcnNe2COe+/G8mdfQ+pXlgjnWDzhsdVktY2kJ5QJZZ9cD5Ru7BOqU2Uqn6Y1PkMA=\"}],\"legacy\":false}");
 
--- src/main/java/com/magmaguy/elitemobs/config/wormholes/premade/AGKnightsCastleWormholeConfig.java
+++ src/main/java/com/magmaguy/elitemobs/config/wormholes/premade/AGKnightsCastleWormholeConfig.java
@@ -8,7 +8,7 @@ public class AGKnightsCastleWormholeConfig extends WormholeConfigFields {
         super("ag_knights_castle_lair_wormhole",
                 true,
                 "em_adventurers_guild,281.5,104.5,302.5,-45,0",
-                "the_knight_castle.yml",
+                "knights_castle_lair.yml",
                 Wormhole.WormholeStyle.ICOSAHEDRON);
         setLocation1Text("&7『Knight's Castle』 &aLvl 95");
         setLocation2Text("&f『Adventurers Guild』");
