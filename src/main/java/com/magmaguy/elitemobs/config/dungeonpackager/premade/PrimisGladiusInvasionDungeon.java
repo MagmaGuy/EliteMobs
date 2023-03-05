dbf2339911082a62c5e53010a7f784d46f1e3dc2
MagmaGuy <tiagoarnaut@gmail.com>
2023-03-05 18:17:30 +0000

    EliteMobs 8.4.0-SNAPSHOT-5 - [New] Added the Primis Gladius Invasion Dungeon - [Fix] Fixed issue that would sometimes cause directories to not be created correctly when importing configurations
    Signed-off-by: MagmaGuy <tiagoarnaut@gmail.com>

---
+++ src/main/java/com/magmaguy/elitemobs/config/dungeonpackager/premade/PrimisGladiusInvasionDungeon.java
@@ -0,0 +1,38 @@
+package com.magmaguy.elitemobs.config.dungeonpackager.premade;
+
+import com.magmaguy.elitemobs.config.dungeonpackager.DungeonPackagerConfigFields;
+import com.magmaguy.elitemobs.utils.DiscordLinks;
+import org.bukkit.World;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class PrimisGladiusInvasionDungeon extends DungeonPackagerConfigFields {
+    public PrimisGladiusInvasionDungeon() {
+        super("primis_gladius_invasion_dungeon",
+                false,
+                "&aGladius Invasion Dungeon",
+                Arrays.asList("&fIt is time to take Gladius back!", "&6Credits: 69OzCanOfBepis, Frostcone, MagmaGuy"),
+                DiscordLinks.premiumMinidungeons,
+                DungeonSizeCategory.DUNGEON,
+                "em_id_primis_gladius",
+                World.Environment.NETHER,
+                true,
+                "em_id_primis_gladius,-108.5,69.0,40.5,-90.499999,0",
+                "em_id_primis_gladius,-100.5,69.0,40.5,-69.499999,0",
+                0,
+                "Difficulty: &4solo hard content!",
+                "&bWelcome to the Gladius Invasion!",
+                "2&bYou have left the Gladius Invasion!",
+                List.of("filename=primis_gladius_bell_id.yml"),
+                "em_id_primis_gladius",
+                1);
+        setDifficulties(List.of(
+                Map.of("name", "normal", "id", 0, "levelSync", "18"),
+                Map.of("name", "hard", "id", 1, "levelSync", "15"),
+                Map.of("name", "mythic", "id", 2, "levelSync", "12")
+        ));
+        setMaxPlayerCount(5);
+    }
+}
