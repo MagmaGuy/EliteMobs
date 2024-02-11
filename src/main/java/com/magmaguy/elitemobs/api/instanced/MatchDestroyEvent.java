ce9ced8cbd873eb822e85007587fd1a7a57716cd
BuildTools <unconfigured@null.spigotmc.org>
2024-02-11 21:05:58 +0000

    Add Match events and interfaces for player instances
    This commit introduces several new classes and interfaces for handling different types of Match events in player instances. These include Join, Leave, Start, End, Destroy, and Instantiate events. Additionally, it enhances the MatchInstance class by initiating these events at appropriate actions such as adding new players, removing players or resetting a match.

---
+++ src/main/java/com/magmaguy/elitemobs/api/instanced/MatchDestroyEvent.java
@@ -0,0 +1,27 @@
+package com.magmaguy.elitemobs.api.instanced;
+
+import com.magmaguy.elitemobs.instanced.MatchInstance;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class MatchDestroyEvent extends Event implements MatchEvent {
+    private static final HandlerList handlers = new HandlerList();
+    private final MatchInstance matchInstance;
+
+    public MatchDestroyEvent(MatchInstance matchInstance) {
+        this.matchInstance = matchInstance;
+    }
+
+    @Override
+    public MatchInstance getInstance() {
+        return matchInstance;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+}
---
+++ src/main/java/com/magmaguy/elitemobs/api/instanced/MatchInstantiateEvent.java
@@ -0,0 +1,38 @@
+package com.magmaguy.elitemobs.api.instanced;
+
+import com.magmaguy.elitemobs.instanced.MatchInstance;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class MatchInstantiateEvent extends Event implements MatchEvent, Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled = false;
+    private final MatchInstance matchInstance;
+
+    public MatchInstantiateEvent(MatchInstance matchInstance) {
+        this.matchInstance = matchInstance;
+    }
+
+    @Override
+    public MatchInstance getInstance() {
+        return matchInstance;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.cancelled = b;
+    }
+}
