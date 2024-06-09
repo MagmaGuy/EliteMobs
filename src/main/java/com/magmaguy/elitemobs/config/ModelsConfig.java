8846907bcb924bd2ecf1d426b173a3d993f209d5
BuildTools <unconfigured@null.spigotmc.org>
2024-06-09 17:09:46 +0100

    - Added sounds config file - Added models config file

---
+++ src/main/java/com/magmaguy/elitemobs/config/ModelsConfig.java
@@ -0,0 +1,23 @@
+package com.magmaguy.elitemobs.config;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.io.File;
+import java.util.List;
+
+public class ModelsConfig {
+    private ModelsConfig(){}
+
+    public static String treasureChest;
+
+    public static void initializeConfig() {
+        File file = ConfigurationEngine.fileCreator("Models.yml");
+        FileConfiguration fileConfiguration = ConfigurationEngine.fileConfigurationCreator(file);
+
+        treasureChest = ConfigurationEngine.setString(
+                List.of("Name of the custom model for the treasure chest"),
+                file, fileConfiguration, "treasureChest", "elitemobs_treasure_chest", false);
+
+        ConfigurationEngine.fileSaverOnlyDefaults(fileConfiguration, file);
+    }
+}
