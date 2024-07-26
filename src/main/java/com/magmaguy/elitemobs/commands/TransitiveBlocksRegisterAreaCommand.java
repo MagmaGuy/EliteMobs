fa24d568af10e10c48a57e848dae4b71df3f9dca
FrostCone <143012927+frostcone@users.noreply.github.com>
2024-07-26 11:05:49 +0200

    Finished going through commands

--- src/main/java/com/magmaguy/elitemobs/commands/TransitiveBlocksRegisterAreaCommand.java
+++ src/main/java/com/magmaguy/elitemobs/commands/TransitiveBlocksRegisterAreaCommand.java
@@ -14,8 +14,8 @@ public class TransitiveBlocksRegisterAreaCommand extends AdvancedCommand {
         addLiteral("registerArea");
         addArgument("filename", CustomBossesConfig.getCustomBosses().keySet().stream().toList());
         addArgument("type", List.of("ON_SPAWN", "ON_REMOVE"));
-        setUsage("/em transitiveBlocks cancel");
-        setPermission("elitemobs.*");
+        setUsage("/em transitiveBlocks registerArea <filename> <ON_SPAWN/ON_REMOVE>");
+        setPermission("elitemobs.transitiveblocks.register.area");
         setDescription("Registers large transitive blocks areas for use by regional bosses.");
         setSenderType(SenderType.PLAYER);
     }
