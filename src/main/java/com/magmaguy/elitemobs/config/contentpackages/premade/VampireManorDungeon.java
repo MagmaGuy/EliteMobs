0cb64b97398a106a466ad5175bd8b50ef7165578
MagmaGuy <tiagoarnaut@gmail.com>
2024-11-11 01:14:17 +0000

    EliteMobs 9.1.12: - [Fix] Fixed issue related to some scripts not triggering correctly - [New] Added SET_FACING action for elite scripts - [New] Added the Vampire Manor remake
    Signed-off-by: MagmaGuy <tiagoarnaut@gmail.com>

---
+++ src/main/java/com/magmaguy/elitemobs/config/contentpackages/premade/VampireManorDungeon.java
@@ -0,0 +1,32 @@
+package com.magmaguy.elitemobs.config.contentpackages.premade;
+
+import com.magmaguy.elitemobs.config.contentpackages.ContentPackagesConfigFields;
+import org.bukkit.World;
+
+import java.util.List;
+
+public class VampireManorDungeon extends ContentPackagesConfigFields {
+    public VampireManorDungeon() {
+        super("vampire_manor_lair",
+                true,
+                "&2[lvl 120] &4The Vampire Manor",
+                List.of("&6Defeat the Vampire King!"),
+                "https://nightbreak.io/plugin/elitemobs/#the-vampire-manor",
+                DungeonSizeCategory.LAIR,
+                "em_the_vampire_manor",
+                World.Environment.NETHER,
+                true,
+                "em_the_vampire_manor,0,64,0,0,0",//todo needs final coords
+                0,
+                "Difficulty: &cHard\n" +
+                        "$bossCount level $lowestTier Big Boss!\n" +
+                        "&6The Vampire King has arisen, stop his summoning!",
+                "&8[EM] &aYou are now trespassing the sacred grove!",
+                "&8[EM] &aYou have left the sacred grove!",
+                "vampire_manor",
+                false);
+        setSetupMenuDescription(List.of(
+                "&2A Lair for players around level 120-140!"
+        ));
+    }
+}
