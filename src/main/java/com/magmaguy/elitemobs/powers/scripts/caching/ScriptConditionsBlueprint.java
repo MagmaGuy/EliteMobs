f3372cad9e0a10f17ddaef8c6dcdc2cebcf52e30
MagmaGuy <tiagoarnaut@gmail.com>
2025-06-20 23:14:47 +0100

    EliteMobs 9.5.0: - [New] Updated for Minecraft 1.21.6 - [New] Script conditions now include a setting to invert optionally invert the check, making it a easier to understand the condition and making inverting conditions possible - [New] Added HEAL action type to elite scripts, which allow elites to heal other elites - [New] Added NEARBY_ELITES as a valid script target - [Fix] Fixed an issue where script relative vectors would sometimes not be interpreted correctly - [Fix] Fixed minor error that could happen when targetting empty script zones - [Fix] Fixed reported security issue with the logify command - [Fix] Fixed warnings related to the logify command
    Signed-off-by: MagmaGuy <tiagoarnaut@gmail.com>

--- src/main/java/com/magmaguy/elitemobs/powers/scripts/caching/ScriptConditionsBlueprint.java
+++ src/main/java/com/magmaguy/elitemobs/powers/scripts/caching/ScriptConditionsBlueprint.java
@@ -18,71 +18,135 @@ public class ScriptConditionsBlueprint {
 
     private final String scriptName;
     private final String filename;
-    @Getter
-    private Boolean isAlive = null;
-    @Getter
-    private List<String> hasTags = null;
-    @Getter
-    private Boolean locationIsAir = null;
-    @Getter
-    private Boolean isOnFloor = null;
-    @Getter
-    private List<String> doesNotHaveTags = null;
-    @Getter
-    private ScriptTargetsBlueprint scriptTargets = null;
-    @Getter
-    private Double randomChance = null;
-    @Getter
-    @Setter
-    private ConditionType conditionType = null;
-    @Getter
-    @Setter
-    private Material isStandingOnMaterial = null;
 
-    //Process from a script
+    @Getter private Boolean isAlive = null;
+    @Getter private Boolean runIfIsAliveIs = true;
+
+    @Getter private List<String> hasTags = null;
+    @Getter private Boolean runIfHasTagIs = true;
+
+    @Getter private Boolean locationIsAir = null;
+    @Getter private Boolean runIfLocationIsAirIs = true;
+
+    @Getter private Boolean isOnFloor = null;
+    @Getter private Boolean runIfIsOnFloorIs = true;
+
+    @Getter private List<String> doesNotHaveTags = null;
+    @Getter private Boolean runIfDoesNotHaveTagIs = true;
+
+    @Getter private ScriptTargetsBlueprint scriptTargets = null;
+    @Getter private Boolean runIfScriptTargetsIs = true;
+
+    @Getter private Double randomChance = null;
+    @Getter private Boolean runIfRandomChanceIs = true;
+
+    @Getter @Setter private ConditionType conditionType = null;
+    @Getter private Boolean runIfConditionTypeIs = true;
+
+    @Getter @Setter private Integer targetCountLowerThan = null;
+    @Getter private Boolean runIfTargetCountLowerThanIs = true;
+
+    @Getter @Setter private Integer targetCountGreaterThan = null;
+    @Getter private Boolean runIfTargetCountGreaterThanIs = true;
+
+    @Getter @Setter private Material isStandingOnMaterial = null;
+    @Getter private Boolean runIfIsStandingOnMaterialIs = true;
+
+    // Process from a script
     public ScriptConditionsBlueprint(ConfigurationSection configurationSection, String scriptName, String filename) {
         this.scriptName = scriptName;
         this.filename = filename;
+
         if (configurationSection.get("Conditions") == null) return;
         Map<?, ?> values = ((ConfigurationSection) configurationSection.get("Conditions")).getValues(false);
         processMapList(values);
-        if (scriptTargets == null) scriptTargets = new ScriptTargetsBlueprint(new HashMap<>(), scriptName, filename);
+
+        if (scriptTargets == null) {
+            scriptTargets = new ScriptTargetsBlueprint(new HashMap<>(), scriptName, filename);
+        }
     }
 
-    //Process from an action
+    // Process from an action
     public ScriptConditionsBlueprint(Map<?, ?> values, String scriptName, String filename) {
         this.scriptName = scriptName;
         this.filename = filename;
-        if (values != null) processMapList(values);
-        if (scriptTargets == null) scriptTargets = new ScriptTargetsBlueprint(new HashMap<>(), scriptName, filename);
+
+        if (values != null) {
+            processMapList(values);
+        }
+
+        if (scriptTargets == null) {
+            scriptTargets = new ScriptTargetsBlueprint(new HashMap<>(), scriptName, filename);
+        }
     }
 
     private void processKeyAndValue(String key, Object value) {
         switch (key.toLowerCase(Locale.ROOT)) {
-            case "isalive" -> isAlive = MapListInterpreter.parseBoolean(key, value, scriptName);
-            case "locationisair" -> locationIsAir = MapListInterpreter.parseBoolean(key, value, scriptName);
-            case "hastags" -> hasTags = MapListInterpreter.parseStringList(key, value, scriptName);
-            case "doesnothavetags" -> doesNotHaveTags = MapListInterpreter.parseStringList(key, value, scriptName);
-            case "isonfloor" -> isOnFloor = MapListInterpreter.parseBoolean(key, value, scriptName);
-            case "isstandingonmaterial" ->
-                    isStandingOnMaterial = MapListInterpreter.parseEnum(key, value, Material.class, scriptName);
-            case "randomchance" -> randomChance = MapListInterpreter.parseDouble(key, value, scriptName);
+            case "isalive" ->
+                    isAlive = MapListInterpreter.parseBoolean(key, value, scriptName);
+            case "runifaliveis" ->
+                    runIfIsAliveIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
+            case "hastags" ->
+                    hasTags = MapListInterpreter.parseStringList(key, value, scriptName);
+            case "runifhastagis" ->
+                    runIfHasTagIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
+            case "locationisair" ->
+                    locationIsAir = MapListInterpreter.parseBoolean(key, value, scriptName);
+            case "runiflocationisairis" ->
+                    runIfLocationIsAirIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
+            case "isonfloor" ->
+                    isOnFloor = MapListInterpreter.parseBoolean(key, value, scriptName);
+            case "runifisonflooris" ->
+                    runIfIsOnFloorIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
+            case "doesnothavetags" ->
+                    doesNotHaveTags = MapListInterpreter.parseStringList(key, value, scriptName);
+            case "runifdoesnothavetagis" ->
+                    runIfDoesNotHaveTagIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
+            case "randomchance" ->
+                    randomChance = MapListInterpreter.parseDouble(key, value, scriptName);
+            case "runifrandomchanceis" ->
+                    runIfRandomChanceIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
+            case "targetcountlowerthan" ->
+                    targetCountLowerThan = MapListInterpreter.parseInteger(key, value, scriptName);
+            case "runiftargetcountlowerthanis" ->
+                    runIfTargetCountLowerThanIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
+            case "targetcountgreaterthan" ->
+                    targetCountGreaterThan = MapListInterpreter.parseInteger(key, value, scriptName);
+            case "runiftargetcountgreaterthanis" ->
+                    runIfTargetCountGreaterThanIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
             case "conditiontype" ->
                     conditionType = MapListInterpreter.parseEnum(key, value, ConditionType.class, scriptName);
+            case "runifconditiontypeis" ->
+                    runIfConditionTypeIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
+            case "isstandingonmaterial" ->
+                    isStandingOnMaterial = MapListInterpreter.parseEnum(key, value, Material.class, scriptName);
+            case "runifisstandingonmaterialis" ->
+                    runIfIsStandingOnMaterialIs = MapListInterpreter.parseBoolean(key, value, scriptName);
+
             case "target" -> {
-                if (value instanceof MemorySection memorySection)
-                    value = memorySection.getValues(false);
-                scriptTargets = new ScriptTargetsBlueprint((Map) value, scriptName, filename);
+                if (value instanceof MemorySection ms) {
+                    value = ms.getValues(false);
+                }
+                scriptTargets = new ScriptTargetsBlueprint((Map<?, ?>) value, scriptName, filename);
             }
-            default -> Logger.warn("Failed to read key " + key + " for script " + scriptName);
+
+            default ->
+                    Logger.warn("Failed to read key " + key + " for script " + scriptName);
         }
     }
 
-    private void processMapList(Map<?, ?> entry) {
-        for (Map.Entry entrySet : entry.entrySet()) {
-            String key = (String) entrySet.getKey();
-            processKeyAndValue(key, entrySet.getValue());
+    private void processMapList(Map<?, ?> entries) {
+        for (Map.Entry<?, ?> entry : entries.entrySet()) {
+            processKeyAndValue(entry.getKey().toString(), entry.getValue());
         }
     }
-
 }
