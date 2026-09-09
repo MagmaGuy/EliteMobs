a18087bf30bee6b43f81e2b926018b46fc33c8b6
MagmaGuy <tiagoarnaut@gmail.com>
2026-09-09 01:59:30 +0100

    WIP: add default Guild patrol NPC with looping route

---
+++ src/main/java/com/magmaguy/elitemobs/config/npcs/premade/GuildPatrolConfig.java
@@ -0,0 +1,35 @@
+package com.magmaguy.elitemobs.config.npcs.premade;
+
+import com.magmaguy.elitemobs.config.npcs.NPCsConfigFields;
+import com.magmaguy.elitemobs.npcs.NPCInteractions;
+import com.magmaguy.elitemobs.pathfinding.patrol.PatrolMode;
+import org.bukkit.entity.Villager;
+
+import java.util.List;
+
+/** Default Guild circuit. Patrol nodes are offsets from the configured spawn. */
+public final class GuildPatrolConfig extends NPCsConfigFields {
+    public GuildPatrolConfig() {
+        super("guild_patrol", true,
+                "<g:#6A7A8A:#8A9AAA>Rowan</g>",
+                "<g:#5A6A7A:#7A8A9A><Guild Patrol></g>",
+                Villager.Profession.ARMORER,
+                "em_adventurers_guild,286.531,92,284.350,0,0",
+                List.of(), List.of(), List.of(), false, 3,
+                NPCInteractions.NPCInteractionType.NONE);
+        setSyncMovement(true);
+    }
+
+    @Override
+    public void processConfigFields() {
+        processBoolean("patrol.enabled", true, false, true);
+        processString("patrol.mode", PatrolMode.LOOP.name(), null, true);
+        processDouble("patrol.speed", 1D, 1D, true);
+        processStringList("patrol.nodes", List.of(
+                "0,0,0",
+                "0.126,0,12.284",
+                "13.895,0,12.206",
+                "14.014,0,0.002"), null, true);
+        super.processConfigFields();
+    }
+}
