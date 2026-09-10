c8621d7b29621f6f90486e8c67003b2deedd14e6
MagmaGuy <tiagoarnaut@gmail.com>
2026-09-10 11:11:02 +0100

    WIP: expose elite target authorization to supplied Lua effects

--- src/main/java/com/magmaguy/elitemobs/scripting/LuaEntityEnricher.java
+++ src/main/java/com/magmaguy/elitemobs/scripting/LuaEntityEnricher.java
@@ -41,6 +41,13 @@ public final class LuaEntityEnricher {
     }
 
     private static void addEliteFields(LuaTable table, Entity entity) {
+        table.set("can_receive_hostile_effect", LuaTableSupport.tableMethod(table, args -> {
+            Entity actor = Bukkit.getEntity(java.util.UUID.fromString(args.checkjstring(1)));
+            return LuaValue.valueOf(actor instanceof org.bukkit.entity.Player player
+                    && LuaEntityTable.isHostileEffectTarget(player, entity)
+                    && com.magmaguy.elitemobs.experimentalcombat.ExperimentalCombatEnemyAuthorization
+                    .canTargetWithMagicWeapon(player, (org.bukkit.entity.LivingEntity) entity));
+        }));
         boolean isElite = EntityTracker.isEliteMob(entity);
         table.set("is_elite", LuaValue.valueOf(isElite));
 
